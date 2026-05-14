c609b7ef91913c73cf80d1485d1797c7be59dafb
EatFan <eatfan0921@163.com>
2026-05-14 13:17:16 +0800

    feat(auth): 添加小程序一键登录功能
    - 新增登录页面组件实现一键授权登录功能
    - 配置登录页面路由路径到应用页面配置文件
    - 设置页面样式支持SCSS预处理器导入机制

---
+++ src/pages/auth/login/index.vue
@@ -0,0 +1,16 @@
+<!-- 小程序一键登录 -->
+<template>
+  <view class="login-page">
+
+    <!--  一键授权登录页面样式模版开发  -->
+
+  </view>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+@import './style';
+</style>
