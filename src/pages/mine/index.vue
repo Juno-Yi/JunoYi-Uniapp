5a56f8c417ba05677acac6e777a383cd3b5e7a78
EatFan <eatfan0921@163.com>
2026-05-15 09:01:48 +0800

    feat(mine): 添加退出登录功能
    - 实现退出登录方法
    - 调整页面显示逻辑，已登录和未登录状态使用v-if/v-else区分
    - 添加退出登录按钮并绑定点击事件
    - 创建body样式容器用于放置退出登录按钮
    - 设置退出登录按钮的边距和边框样式

--- src/pages/mine/index.vue
+++ src/pages/mine/index.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { computed } from 'vue'
 import { useUserStore } from '@/stores'
+import {fetchLogout} from "@/api/auth";
 
 const userStore = useUserStore()
 
@@ -16,6 +17,35 @@ const goToLogin = () => {
     url: '/pages/auth/login/index'
   })
 }
+
+/**
+ * 退出登录
+ */
+const logout = async () => {
+  try {
+    // 先调用退出登录接口
+    await fetchLogout();
+    // 再清楚本地状态数据
+    userStore.logout();
+    uni.showToast({
+      title: '退出成功',
+      icon: 'success'
+    });
+    // 跳转到主页
+    setTimeout(() => {
+      uni.reLaunch({
+        url: '/pages/home/index'
+      });
+    }, 1000)
+  } catch (error){
+    console.error('退出登录失败:', error);
+    // 即使后端失败，也清除本地状态
+    userStore.logout();
+    uni.reLaunch({
+      url: '/pages/home/index'
+    });
+  }
+}
 </script>
 
 <template>
@@ -29,15 +59,20 @@ const goToLogin = () => {
           <text>【调试】：未登录</text>
           <button @click="goToLogin">登录</button>
         </view>
-      </view>
 
-      <!-- 用户登录 -->
-      <view class="is-login" v-if="userStore.isLogin">
+        <!-- 用户登录 -->
+        <view class="is-login" v-else>
           <text>【调试】：已登录</text>
           <u-avatar :src="displayAvatar"></u-avatar>
           <text>用户昵称：{{ displayName }}</text>
           <text>用户手机号：{{ displayPhone }}</text>
+        </view>
       </view>
+
+      <view class="body">
+        <button @click="logout">退出登录</button>
+      </view>
+
     </view>
   </app-page>
 </template>
